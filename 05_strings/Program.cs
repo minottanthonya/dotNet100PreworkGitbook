308a09c72c8234659395d996acc5e78e94008636
Tony Minott <minottanthonya@gmail.com>
2018-08-14 10:51:59 -0400

    Better comments and better identification of the three types of string usage.

--- 05_strings/Program.cs
+++ 05_strings/Program.cs
@@ -13,16 +13,22 @@ namespace _05_strings
 
             string wholeName = "Douglas Crockford";
 
+            //Concatenation:
+
             string first = "The cars we sell are ";
             string second = "BMW, Lexus, and Mercedes.";
             Console.WriteLine(first + second);
             Console.WriteLine(first + " " + second);
 
+            //Composite Formatting:
+
             string firstName = "Jenn";
             string lastName = "Wilson";
-            // Composite Formatting
+          
             Console.WriteLine("Her name is {0} {1}.", firstName, lastName);
-            // String Interpolation
+            
+            // String Interpolation:
+
             Console.WriteLine($"Her name is {firstName} {lastName}");
 
 

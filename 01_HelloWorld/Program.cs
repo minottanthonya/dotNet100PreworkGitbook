073a7ee7dd31c04fc89e78f417a24f79d67d7800
Tony Minott <minottanthonya@gmail.com>
2018-08-14 10:23:20 -0400

    Comment for the first area

--- 01_HelloWorld/Program.cs
+++ 01_HelloWorld/Program.cs
@@ -10,13 +10,19 @@ namespace _01_HelloWorld
     {
         static void Main(string[] args)
         {
+
+            // Challenge
+
+            //To get a little bit of practice running the console and writing in C#, 
+            //create a conversation between two people within your console application, 
+            //and underneath the "Hello World" message. It should look something like this:
+            
             Console.WriteLine("Hello World");
 
             Console.WriteLine("Hey there how are you doing?");
             Console.WriteLine("I'm good, how are you?");
             Console.WriteLine("I'm fantastic, I'm learning how to code!");
             Console.WriteLine("Wow! That's great!");
-
 
             Console.ReadLine();  
         }
